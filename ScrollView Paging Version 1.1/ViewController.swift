3c0bafe0f0fd51dfea5cde4221e1cb992bbf592c
Viet Anh Doan <vtahdn@icloud.com>
2017-07-12 23:49:25 +0700

    Initial Commit

---
+++ ScrollView Paging Version 1.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollView Paging Version 1.1
+//
+//  Created by Viet Anh Doan on 7/12/17.
+//  Copyright © 2017 Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
